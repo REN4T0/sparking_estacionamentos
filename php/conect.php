ce257b23b2773915e6c20a5d31780fef1373e42d
Renato Viturino <95659541+REN4T0@users.noreply.github.com>
2023-04-24 14:36:16 -0300

    Update conect.php

--- php/conect.php
+++ php/conect.php
@@ -4,10 +4,10 @@
 // $senha = 'tN3JLRmxF7wbXK';
 // $dbname = 'epiz_33943789_sparking';
 
-    $servidor = 'localhost';
-    $usuario = 'root';
-    $senha = '';
-    $dbname = 'sparking';
+    $servidor = 'sitebcd-server.mysql.database.azure.com';
+    $usuario = 'liiaqgapzp';
+    $senha = 'B5JDER53I5E76M68$';
+    $dbname = 'sitebcd-database';
 
     $conn = mysqli_connect($servidor, $usuario, $senha, $dbname);
 ?>
